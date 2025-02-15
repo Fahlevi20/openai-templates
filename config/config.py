fdaa3a07cff5daee67877bd2012cb62e5ef89f78
fahlevi20 <rozakalfi@gmal.com>
2025-02-15 21:28:59 +0700

    add model config

--- config/config.py
+++ config/config.py
@@ -1,6 +1,10 @@
 from openai import OpenAI
+from model_config import Model
+import os
+
+model_name= os.environ.get("MODEL_NAME")
 
 client = OpenAI()
 completion= client.chat.completions.create(
-    
+    model=Model(model_name)
 )
