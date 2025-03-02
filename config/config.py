78cc169a1e9cb72622a9306d994a0a00bd80993e
fahlevi20 <rozakalfi@gmal.com>
2025-03-02 11:00:28 +0700

    - Created 'model_configuration' function in 'config.py' to allow flexible model parameter adjustments. - Created 'chat_message' function in 'chat.py' to manage conversation state and generate appropriate response.
    - Erstellt "model_configuration'-Funktion in 'chat.py' flexible erlauben  Modellparameter-Anpassungen
    - Erstellt 'chat_message'-Funktion in 'chat.py' konversationsstatus verwalten und angemessen reaktion generieren

--- config/config.py
+++ config/config.py
@@ -1,10 +1,17 @@
 from openai import OpenAI
 from model_config import Model
 import os
+from chat.chat import chat_message
 
 model_name= os.environ.get("MODEL_NAME")
+api_key = os.environ.get("API_KEY")
+organization_id=os.environ.get("ORGANIZATION_ID")
 
-client = OpenAI()
-completion= client.chat.completions.create(
-    model=Model(model_name)
-)
+def model_configuration():
+    client = OpenAI(api_key=api_key)
+    completion= client.chat.completions.create(
+        model=Model(model_name),
+        messages=chat_message
+    )
+    
+    return client.choices[0].messages
---
+++ main.py
@@ -0,0 +1,3 @@
+from config.config import model_configuration
+
+print(model_configuration.choices[0].messages)
